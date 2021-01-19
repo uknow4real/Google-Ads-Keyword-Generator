c79cb9ecb86f38ff27b324913fbf079404d2f31b
Sebastian Chmel <sebastian.chmel@stud.fh-campuswien.ac.at>
2021-01-19 02:05:16 +0100

    working keyword generator

---
+++ generator.py
@@ -0,0 +1,25 @@
+from typing import Generator
+from datetime import date, datetime
+import os
+
+def generator(number):
+    typebeats = "{} type beat\nfree {} type beat\n{} type beat 2021\nfree {} type beat 2021\n{} hard type beat\n{} hard type beat 2021\n"
+    today = datetime.now()
+    d1 = today.strftime("%d_%m_%Y-%H:%M:%S")
+    filename = d1 +".csv"
+    dirname = os.path.dirname(filename)
+    if os.path.exists(filename):
+        append_write = 'a' # append if already exists
+    else:
+        append_write = 'w' # make a new file if not
+    f = open("keywords/"+filename, append_write)
+    i = 1
+    for x in range (number):
+        artist = input("Enter " + str(i) + ". Artist: ")
+        result = typebeats.format(artist, artist, artist, artist, artist, artist, 6)
+        f.write(result)
+        i = int(i) + 1 
+    f.close
+    
+number = int(input("Number of Artists: "))
+generator(number)
